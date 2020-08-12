184c24d5e3c01c8d003f5455c89889d43172671f
robscott1 <robertdscott24@gmail,com>
2020-08-11 22:18:25 -0700

    comments and first python lambda

--- amplify/backend/function/cleanandcategorize/src/index.py
+++ amplify/backend/function/cleanandcategorize/src/index.py
@@ -1,5 +1,30 @@
 import json
+
+class Transaction():
+  # This object will pull out the items we want for 
+  # analyses. For any future work, this can be modified
+  # to hold the info we want
+
+  def __init__(self, amt, date, name, merchant_name, category='unhandled'):
+    self.amt = amt
+    self.date = date
+    self.name = name
+    self.merchantName = merchant_name
+    self.category = category
+    
+  def toJSON(self):
+    return json.dumps(self, default=lambda x: x.__dict__, indent=2)
+
 def handler(event, context):
+
+  cleaned = []
+  for key, value in event.items():
+    amt = value['amount']
+    date = value['date']
+    name = value['name']
+    merchant_name = value['merchant_name']
+    t = Transaction(amt, date, name, merchant_name)
+    cleaned.append(t.toJSON())
 
   msg = {
     'statusCode': 200,
@@ -8,7 +33,7 @@ def handler(event, context):
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
     },
-    'body': json.dumps('hello from lambda')
+    'body': json.dumps({'cleaned' : cleaned})
   }
 
   return msg
