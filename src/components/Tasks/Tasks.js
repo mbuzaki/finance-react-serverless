9bd4d81695e34b419fb8eb85c393acffe551d89f
Rob Scott <rdavidscott1@gmail.com>
2020-08-28 20:00:11 -0700

    add drag/drop abilities

--- src/components/Tasks/Tasks.js
+++ src/components/Tasks/Tasks.js
@@ -1,50 +1,44 @@
-import React from "react";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
 import classnames from "classnames";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
-import Checkbox from "@material-ui/core/Checkbox";
-import Tooltip from "@material-ui/core/Tooltip";
-import IconButton from "@material-ui/core/IconButton";
 import Table from "@material-ui/core/Table";
 import TableRow from "@material-ui/core/TableRow";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
-// @material-ui/icons
-import Edit from "@material-ui/icons/Edit";
-import Close from "@material-ui/icons/Close";
-import Check from "@material-ui/icons/Check";
+
 // core components
 import styles from "assets/jss/material-dashboard-react/components/tasksStyle.js";
+import { store } from "../../store.js";
+import { onDrop } from './functions/onDrop.js'
 
 const useStyles = makeStyles(styles);
 
 export default function Tasks(props) {
   const classes = useStyles();
-  // const [checked, setChecked] = React.useState([...props.checkedIndexes]);
-  // const handleToggle = value => {
-  //   const currentIndex = checked.indexOf(value);
-  //   const newChecked = [...checked];
-  //   if (currentIndex === -1) {
-  //     newChecked.push(value);
-  //   } else {
-  //     newChecked.splice(currentIndex, 1);
-  //   }
-  //   setChecked(newChecked);
-  // };
+  const userInfo = useContext(store)
+
+  const onDragOver = (e) => {
+    e.preventDefault();
+  }
+
   const { tasksIndexes, tasks, rtlActive } = props;
   const tableCellClasses = classnames(classes.tableCell, {
     [classes.tableCellRTL]: rtlActive
   });
+
   return (
     <Table className={classes.table}>
-      <TableBody>
+      <TableBody onDragOver={(e) => {onDragOver(e)}}
+                  onDrop={(e) => {onDrop(e, props.categoryIndex, userInfo)}}>
         {tasks.map((value) => {
             /* Renders a row with the important information 
             for each transaction. This exists in both 
             sorted and unhandled tables.*/
             return (
               <TableRow>
+                <TableCell>{value.id}</TableCell>
                 <TableCell>{value.name}</TableCell>
                 <TableCell>{value.date}</TableCell>
                 <TableCell>{value.category}</TableCell>
